18c3c48e01e46b936000ef6f5ebaaf7abc324888
Maicol Hernandez <maykolandres4@gmail.com>
2025-06-03 14:00:54 -0400

    added struct, and created get post

--- main/src/main.rs
+++ main/src/main.rs
@@ -16,13 +16,15 @@ async fn main() -> Result<(), sqlx::Error> {
     
     dotenv().ok();
     let url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let _pool = PgPoolOptions::new().connect(&url).await?;
+    let pool = PgPoolOptions::new().connect(&url).await?;
     println!("connect to database");
 
     // build application with router
     let app = Router::new()
         // get goes to root
         .route("/", get(root));
+        // axum extension layer
+        .layer(Extension(pool));
 
     //run app with hyper that listening globally to our port
     //run server on 5001, 5000 is busy
@@ -36,4 +38,23 @@ async fn main() -> Result<(), sqlx::Error> {
 // handler for GET
 async fn root() -> &'static str {
     "hello, world"
+}
+
+#[derive(Serialize, Deserialize)]
+struct Post {
+    id: i32,
+    user_id: Option<i32>,
+    title: String,
+    body: String,
+}
+
+async fn get_posts(
+    Extension(pool): Extension<Pool<Postgres>>
+) -> Result<Json<Vec<Post>>, StatusCode> {
+    let posts = sqlx::query_as!(Post, "SELECT id, title, body FROM posts")
+        .fetch_all(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(Json(posts))
 }
