c293e8f1711705ca67386756327c49f5a3767552
Maicol Hernandez <maykolandres4@gmail.com>
2025-11-09 11:25:04 -0500

    fix: fix type, and fix port number

--- main/src/config.rs
+++ main/src/config.rs
@@ -9,15 +9,15 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn from_env() -> Result<Self, env:VarError> {
+    pub fn from_env() -> Result<Self, env::VarError> {
         Ok(Self {
-            spotify_client_id: env::var("SPOTIFY_CLIENT_ID")?.
+            spotify_client_id: env::var("SPOTIFY_CLIENT_ID")?,
             spotify_client_secret: env::var("SPOTIFY_CLIENT_SECRET")?,
             spotify_refresh_token: env::var("SPOTIFY_REFRESH_TOKEN")?,
             port: env::var("PORT")
                 .unwrap_or_else(|_| "8080".to_string())
                 .parse()
-                .unwrap_or(808j),
+                .unwrap_or(8080),
         })
     }
 }
