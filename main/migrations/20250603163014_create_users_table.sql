2aacb22f4ea2b794d102e9112abc6f6956f6b44c
Maicol Hernandez <maykolandres4@gmail.com>
2025-11-09 01:38:15 -0500

    delete: reset

--- main/migrations/20250603163014_create_users_table.sql
+++
@@ -1,8 +0,0 @@
- -- migrations/<timestamp>_create_users_table.sql
- 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW()
-);
--- main/migrations/20250603163046_create_posts_table.sql
+++
@@ -1,9 +0,0 @@
--- migrations/<timestamp>_create_posts_table.sql
- 
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
